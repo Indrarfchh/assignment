ef81abfba5014339dbb40a8e310f9b8f68ba8218
Indrarfchh <indrakanth.rfchh@gmail.com>
2024-10-01 16:23:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import Project from './project';
+import App from './App';
+
 // import DynamicTable from './assignment';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
     <Navbar/>
-    <App />
-    {/* <Project/> */}
-  </React.StrictMode>
+    <Routes>
+    <Route path="/" element={<Project />} />
+    <Route path="/App" element={<App />} />
+    </Routes>
+  </Router>
 );
 
